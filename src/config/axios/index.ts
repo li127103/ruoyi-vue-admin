7a1b14a8a86b0fe939e45df3c4d9a805a358e887
v_liyu6 <v_liyu6@tal.com>
2026-01-29 18:32:22 +0800

    feat: 加解密完成

---
+++ src/config/axios/index.ts
@@ -0,0 +1,14 @@
+
+
+const  request = (option: any) =>{
+    const { headersType, headers, ...otherOption } = option
+    return
+
+}
+
+
+export  default  {
+    get: async<T = any>(option: any) =>{
+        const  res = await req
+}
+}

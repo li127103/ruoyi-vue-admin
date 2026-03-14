825213521492418a3f4b09a19d1f57150f261486
xiaoyu <1278707455@qq.com>
2026-03-14 22:48:19 +0800

    feat: Backtop 组件完成

---
+++ src/components/ColorInput/index.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import {propTypes} from '@/utils/propTypes'
+import {PREDEFINE_COLORS} from '@/utils/color'
+
+defineOptions(
+    {name: 'ColorInput'}
+)
+
+const props = defineProps({
+  modelValue: propTypes.string.def('')
+})
+
+const emit = defineEmits(['update:modelValue'])
+const color = computed({
+  get: () => {
+    return props.modelValue
+  },
+  set: (val: string) => {
+    emit('update:modelValue', val)
+  }
+})
+</script>
+:deep(.el-input-group__prepend) {
+padding: 0;
+}
+<template>
+  <el-input v-model="color">
+    <template #prepend>
+      <el-color-picker v-model="color" :predefine="PREDEFINE_COLORS"/>
+    </template>
+  </el-input>
+</template>
+
+<style scoped lang="scss">
+
+</style>
